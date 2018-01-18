82dd2ab6ad68901127ab90bb7f39b04432e73a7f
Shuang <lishuang2009@gmail.com>
2018-01-18 11:25:30 -0600

    Updated Dockerfile_GPU and setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(
-    name='learning_plasticity',
+    name='Plasticity_Ker',
     version='0.0.0',
     description='Next generation learning plasticity model',
     author='Shuang Li',
     author_email='shuang.li@bcm.edu',
-    url='https://github.com/ShuangLiBCM/learning_plasticity',
+    url='https://github.com/ShuangLiBCM/Plasticity_Ker',
     packages=find_packages(exclude=[]),
     install_requires=['numpy'],
 )
