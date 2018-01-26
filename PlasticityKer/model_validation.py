f9925ccab7978a99343c9c18fced8dc7d44a4b5a
Shuang <lishuang2009@gmail.com>
2018-01-26 15:26:11 -0600

    Create model validation script, removed atflow from Dockerfile GPU

---
+++ PlasticityKer/model_validation.py
@@ -0,0 +1,27 @@
+"""
+    Perform model validation for two network (PairNet, TripNet) and 3 datasets (STDP alone, Hippocampal data,
+    Visual cortex data). Save the network and best parameters to specific folder in /model"
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+from modelval import pairptl, network, trainer, dataset
+from modelval.ArbDataGen import arb_w_gen
+
+# Generate kernel
+
+# Load data
+
+# Generate data of specific protocol (Train, targets)
+
+# Build the network
+
+# Create the trainer
+
+# Train the model
+
+# Save the model and best parameter
+
