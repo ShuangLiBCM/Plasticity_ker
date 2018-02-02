d83108c9c533238afba8a20405946826bdaeb3aa
Shuang <lishuang2009@gmail.com>
2018-02-01 22:17:36 -0600

    Added triplet model

--- PlasticityKer/modelval/dw_gen.py
+++ PlasticityKer/modelval/dw_gen.py
@@ -37,4 +37,42 @@ def STDP_dw_gen(n_neighbors=3, df=None):
     Kn_reg.fit(x, y)
     y_pred = Kn_reg.predict(np.array(df['dt1']).reshape(-1, 1))
 
-    return y_pred
+    return df, y_pred
+
+def triplet_dw_gen(dt=None):
+
+    data = pd.read_csv('/src/Plasticity_Ker/data/kernel_training_data_auto.csv')
+    data2 = data[data['ptl_idx'] == 2]
+    data4 = data[data['ptl_idx'] == 4]
+
+    if dt is None:
+        dt = np.array([-10, -5, 0, 5, 10]).reshape(-1, 1)
+
+    data2_gen = pd.DataFrame(data=None, columns=list(data.columns))
+
+    for i in range(len(dt)):
+        new_try2 = data2.iloc[0]
+        if dt[i] < 0:
+            new_try2['dt1'] = dt[i]
+            new_try2['dt2'] = dt[i]
+            new_try2['dw_mean'] = data2[data2['dt1'] < 0]['dw_mean'].mean()
+        if dt[i] == 0:
+            new_try2['dt1'] = dt[i]
+            new_try2['dt2'] = dt[i]
+            new_try2['dw_mean'] = 60
+        elif dt[i] > 0:
+            new_try2['dt1'] = dt[i]
+            new_try2['dt2'] = dt[i]
+            new_try2['dw_mean'] = data2[data2['dt1'] > 0]['dw_mean'].mean()
+
+        data2_gen = data2_gen.append(new_try2, ignore_index=True)
+
+    targets2 = np.array(data2_gen['dw_mean']).reshape(-1, 1)
+
+    data4_gen = data4
+    targets4 = np.array(data3['dw_mean']).reshape(-1, 1)
+
+    data_gen = pd.concat([data2_gen, data4_gen])
+    targets = np.concatenate([targets2, targets4])
+
+    return data_gen, targets
