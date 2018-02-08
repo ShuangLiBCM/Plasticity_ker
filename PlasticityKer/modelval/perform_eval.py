8811df53305647fd525562a83be9346e06ca13d0
Shuang <lishuang2009@gmail.com>
2018-02-08 14:20:04 -0600

    Implemented spike visualization module

--- PlasticityKer/modelval/perform_eval.py
+++ PlasticityKer/modelval/perform_eval.py
@@ -4,6 +4,7 @@
 import numpy as np
 
 def R2_corr(predictions, targets, x_fit):
+
     R2 = 1 - np.sum(np.square(predictions - targets)) / np.sum(np.square(targets - np.mean(targets)))
     # Calculate correlation coefficient
     corr = np.sum(np.dot((predictions - np.mean(predictions)).T, targets - np.mean(targets))) / np.std(
@@ -11,12 +12,6 @@ def R2_corr(predictions, targets, x_fit):
     b1 = np.sum(np.dot((predictions - np.mean(predictions)).T, targets - np.mean(targets))) / np.sum(
         np.square(targets - np.mean(targets)))
     b2 = np.mean(predictions) - np.mean(targets) * b1
-    # x_fit = np.linspace(np.min(targets) - 1, np.max(targets) + 1, 100)
     y_fit = x_fit * b1 + b2
-    # plt.plot(x_fit, y_fit, 'k--')
-    # plt.plot(targets, predictions, 'ro')
-    # plt.xlabel('targets')
-    # plt.ylabel('predictions')
-    # plt.title(''.join(('R2=%.4f' % (R2), ', Corr=%.4f' % (corr))))
     return R2, corr, y_fit
 
---
+++ PlasticityKer/modelval/spk_visu.py
@@ -0,0 +1,52 @@
+"""
+Visualize spike train raster plots of a certain protocol
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+def spk_see(ptl_type=1, spk_pairs):
+
+    if ptl_type == 1:
+        # Obtain the pre-post scatter plot
+        loci_pre = []
+        loci_post = []
+        dt_mean = []
+        for i in range(spk_pairs.shape[0]):
+            loci_pre_tmp = np.where(spk_pairs[i, :, 0] == 1)[0]
+            loci_post_tmp = np.where(spk_pairs[i, :, 1] == 1)[0]
+            loci_pre.append(loci_pre_tmp)
+            loci_post.append(loci_post_tmp)
+            dt_mean.append(np.mean(loci_pre_tmp - loci_post_tmp))
+
+        sort_index = np.argsort(dt_mean)
+        loci_pre_2 = [loci_pre[i] for i in sort_index]
+        loci_post_2 = [loci_post[i] for i in sort_index]
+
+    elif (ptl_type == 2) | (ptl_type == 4):
+        pass
+    elif ptl_type == 3:
+        pass
+    else:
+        pass
+
+    return loci_pre_2, loci_post_2
+
+
+def raster(event_times_list, trial_length,  **kwargs):
+    """
+    Creates a raster plot
+    Parameters
+    ----------
+    event_times_list : iterable
+                       a list of event time iterables
+    color : string
+            color of vlines
+    Returns
+    -------
+    ax : an axis containing the raster plot
+    """
+    ax = plt.gca()
+    for ith, trial in enumerate(event_times_list):
+        plt.vlines(trial[trial_length]*2, ith + .5, ith + 1.5, **kwargs)
+    plt.ylim(.5, len(event_times_list) + .5)
+    return ax
