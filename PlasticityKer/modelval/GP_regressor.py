c21ebb93114b23feca59f128897e9c3670c5ca00
Shuang <lishuang2009@gmail.com>
2018-02-21 15:34:38 -0600

    Improved comments over Gaussian Process Regressor

--- PlasticityKer/modelval/GP_regressor.py
+++ PlasticityKer/modelval/GP_regressor.py
@@ -11,8 +11,15 @@ def gp_regessor(x, y, x_test, sigma_obs=1, sigma_kernel=1, bias=1, noise_const=1
     :param x_test: x axis of testing data, k data points with d dimension each (k,d)
     :param sigma_obs:
     :param sigma_kernel:
-    :param if_nonstationary: whether to enable the nonstationary component
+    :param if_stat_kernel: if the fitting use stationary prior covariance matrix, default is yes
+    :param if_stat_noise: if the fitting use stationary prior noise, default is yes
     :return:
+    f: mean of the posterior distribution over function
+    v_f: covariance of the posterior distribution over the function
+    lp: log likelihood of posterior distribution of function over training data
+
+    Refer to book "Gaussian Processes for Machine Learning" algorithm 2.3
+
     """
     y_bias = np.squeeze(np.mean(y, axis=0))    # Assume zeros mean, remove bias first, add it back later
     
@@ -20,6 +27,7 @@ def gp_regessor(x, y, x_test, sigma_obs=1, sigma_kernel=1, bias=1, noise_const=1
     K_test = cov_kernel(x_test, x, sigma_kernel,bias, scale, if_stat_kernel)
     
     if not if_stat_noise:
+        # Implement non-stationary noise that dependents on absolute value of x
         sigma_noise = sigma_obs*np.exp(-1 * np.abs(x)/noise_const)
     else:
         sigma_noise = sigma_obs
@@ -47,6 +55,7 @@ def cov_kernel(x1, x2, prec, bias, scale, if_stat_kernel=True):
             len_scale = np.abs(x1[i, :])
         else:
             len_scale = 1
-        K[i, :] = scale * np.exp(-0.5 * np.square(dist/(prec*np.sqrt(len_scale)+bias))).transpose() # Sum over the dimension d
+        # Sum over the dimension d
+        K[i, :] = scale * np.exp(-0.5 * np.square(dist/(prec*np.sqrt(len_scale)+bias))).transpose()
 
     return K
